94422d921e36f1f570d2bc6620f0cb6bfb05fd44
minhchienazxd <chienkim65@gmail.com>
2024-03-21 19:18:49 +0700

    c

--- BaiThucHanh/src/ConNguoi/mainPerson.java
+++ BaiThucHanh/src/ConNguoi/mainPerson.java
@@ -2,12 +2,11 @@ package ConNguoi;
 
 public class mainPerson {
     public static void main(String[] args) {
-        person cn = new person();
-        System.out.println("ten: " + cn.ten);
-        System.out.println("Tuoi: " + cn.tuoi);
-        System.out.println("sdt: " + cn.sdt);
-        cn.nhapThongTin();
-        cn.InThongTin();
+        employee_fulltime e = new employee_fulltime();
+        e.persons();
+        e.employees();
+        e.employee_fulltimes();
+       
     }  
     
 }
--- BaiThucHanh/src/ConNguoi/person.java
+++ BaiThucHanh/src/ConNguoi/person.java
@@ -1,36 +1,31 @@
 package ConNguoi;
-import java.util.Scanner;;
-public class person {
-    public String ten;
-    public int tuoi;
-    public String sdt;
-
-    public person() {
-
-        ten = "chien";
-        tuoi = 18;
-        sdt = "27462222222222228";
-    }
-    
-
-    public void nhapThongTin() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("ten: ");
-        ten = scanner.nextLine();
-        System.out.println("tuoi: ");
-        tuoi = scanner.nextInt();
-        System.out.println("sdt: ");
-        sdt = scanner.nextLine();
-        
-        scanner.close();
-
-    }
-    public void InThongTin() {
+class person {
+    String ten = "chien";
+    float tuoi = 19;
+    String sdt = "281931381783";
+    void persons() {
         System.out.println("ten: " + ten);
         System.out.println("tuoi: " + tuoi);
         System.out.println("sdt: " + sdt);
-       
         
     }
-    
 }
+class employee extends person {
+    float luong = 1000;
+    void employees() {
+        System.out.println("luong: " + luong);
+    }
+}
+class employee_pasttime extends employee {
+    int bonus = 0;
+    void employee_pasttimes() {
+        System.out.println("bonus: " + bonus);
+    }
+}
+class employee_fulltime extends employee {
+    int bonus = 100;
+    void employee_fulltimes() {
+        System.out.println("bonus: " + bonus);
+    }
+}
+
